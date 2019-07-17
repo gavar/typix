fc870112457bab8a8a066816a7ca308bb52d7b1a
Max Stankevich <max.stankevich.me@gmail.com>
2019-07-17 07:44:11 +0300

    fix: minor lint optimizization

--- packages/eslint-config/airbnb-base/import.js
+++ packages/eslint-config/airbnb-base/import.js
@@ -1,11 +1,12 @@
 module.exports = {
   rules: {
+    "import/named": "off", // TODO: enable, disabled due to poor performance
+    "import/no-cycle": "off", // TODO: enable, disabled due to poor performance
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true, optionalDependencies: false }],
     "import/order": ["error", {
       "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
       "newlines-between": "ignore", // FIXME: set to never, conflicts with: import/newline-after-import + require
     }],
-    "import/no-cycle": "error",
-    "import/no-extraneous-dependencies": ["error", { devDependencies: true, optionalDependencies: false }],
     "import/prefer-default-export": "off",
   },
 };
